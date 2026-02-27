37ed6b7ae57b8daa174ccee2df9e6ebc943290a9
teaqu <teaqu@outlook.com>
2026-02-27 00:58:16 +0000

    e2e alpha test

--- rendering/vitest.config.ts
+++ rendering/vitest.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
+    exclude: [...configDefaults.exclude, 'src/test/e2e/**'],
     coverage: {
       exclude: [
         'vendor/**',
